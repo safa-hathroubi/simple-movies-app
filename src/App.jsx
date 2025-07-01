e10287d8d524a67a1f3a1f3fdf3aa6ab9f83a7ce
safa-hathroubi <safa.hathroubi@esprit.tn>
2025-07-01 08:56:58 +0200

    added movie-card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from "react";
 import Search from "./components/Search.jsx";
 import Spinner from "./components/Spinner.jsx";
+import MovieCard from "./components/MovieCard.jsx";
 
 const API_BASE_URL = "http://api.themoviedb.org/3/";
 
@@ -73,7 +74,7 @@ const App = () => {
                         ) : errorMessage ? (<p className="text-red-500">{errorMessage}</p>) : (
                             <ul>
                                 {movieList.map((movie) => (
-                                    <p key={movie.id} className="text-white">{movie.title}</p>
+                                    <MovieCard key={movie.id} movie={movie}/>
                                 ))}
                             </ul>
                         )}
