da810296aa5b19db0390a0e2bda036555c80cdec
safa-hathroubi <safa.hathroubi@esprit.tn>
2025-07-01 10:20:21 +0200

    added getTrendingMovies from appwrite

--- src/appwrite.js
+++ src/appwrite.js
@@ -36,3 +36,17 @@ export const updateSearchCount = async (searchTerm, movie) => {
         console.error(e);
     }
 }
+
+export const getTrendingMovies = async () => {
+    try {
+        const result = await database.listDocuments(DATABASE_ID, COLLECTION_ID, [
+            Query.limit(5),
+            Query.orderDesc("count")
+        ])
+
+        return result.documents;
+
+    } catch (e) {
+        console.error(e);
+    }
+}
